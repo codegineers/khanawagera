533aaa5f5c17bf2998fc9ad0fdad168a6ba1d6d5
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-15 21:02:00 +0500

    chore(admin): prettier format files

--- apps/admin/app/routes/restaurants/$restaurantId/menu/$menuId.jsx
+++ apps/admin/app/routes/restaurants/$restaurantId/menu/$menuId.jsx
@@ -2,40 +2,44 @@ import { Outlet, Link, useSearchParams, useLoaderData } from '@remix-run/react'
 import { json, redirect } from '@remix-run/node'
 
 import Button from '~/components/Button'
-import AddDishDialog from '~/dialogs/AddDish';
+import AddDishDialog from '~/dialogs/AddDish'
 
 import { createDish, getDishes } from '~/models/dish.sever'
 
 export async function action({ request, params }) {
-  const { menuId, restaurantId } = params
-  const formData = await request.formData()
-  const name = formData.get("dish")
-  await createDish({ name, menuId })
-  return redirect(`/restaurants/${restaurantId}/menu/${menuId}`)
+	const { menuId, restaurantId } = params
+	const formData = await request.formData()
+	const name = formData.get('dish')
+	await createDish({ name, menuId })
+	return redirect(`/restaurants/${restaurantId}/menu/${menuId}`)
 }
 
 export async function loader({ params }) {
-  const { menuId } = params
-  const dishes = await getDishes(menuId)
-  return json({ dishes })
+	const { menuId } = params
+	const dishes = await getDishes(menuId)
+	return json({ dishes })
 }
 
 export default function RestaurantMenu() {
-  const { dishes } = useLoaderData()
-  const [searchParams] = useSearchParams();
-  const showNewDishModal = searchParams.get("new-dish");
-  return (
+	const { dishes } = useLoaderData()
+	const [searchParams] = useSearchParams()
+	const showNewDishModal = searchParams.get('new-dish')
+	return (
 		<div>
-      <div>
-        {dishes.map(dish => (
-          <div key={dish.id} className="w-56 my-2 rounded">{dish.name}</div>
-        ))}
-      </div>
-      <Link to="?new-dish=true">
-        <Button type="button" primary>Add Dish</Button>
-      </Link>
+			<div>
+				{dishes.map((dish) => (
+					<div key={dish.id} className="w-56 my-2 rounded">
+						{dish.name}
+					</div>
+				))}
+			</div>
+			<Link to="?new-dish=true">
+				<Button type="button" primary>
+					Add Dish
+				</Button>
+			</Link>
 			<Outlet />
-      {showNewDishModal && <AddDishDialog title={"Add dish"} />}
+			{showNewDishModal && <AddDishDialog title={'Add dish'} />}
 		</div>
 	)
 }
--- apps/admin/app/routes/restaurants/index.jsx
+++ apps/admin/app/routes/restaurants/index.jsx
@@ -1,38 +1,38 @@
-import { useLoaderData, Link, Outlet } from "@remix-run/react";
-import { json } from "@remix-run/node";
+import { useLoaderData, Link, Outlet } from '@remix-run/react'
+import { json } from '@remix-run/node'
 
-import { getRestaurants } from "~/models/restaurant.server";
+import { getRestaurants } from '~/models/restaurant.server'
 
-import Button from "~/components/Button";
+import Button from '~/components/Button'
 
 export async function loader() {
-  return json(await getRestaurants());
+	return json(await getRestaurants())
 }
 
 export default function RestaurantPage() {
-  const restaurants = useLoaderData()
+	const restaurants = useLoaderData()
 
-  return (
-    <div className="container px-8">
-      <h2 className="mt-2 text-lg font-semibold text-slate-700">Restaurants</h2>
-      
-      <div className="my-4">
-        {restaurants.map(({id, name}) => (
-        <div key={`${id}-${name}`}>
-          <Link to={`${id}/cuisine`}>{name}</Link>
-        </div>
-      ))}
-      </div>
+	return (
+		<div className="container px-8">
+			<h2 className="mt-2 text-lg font-semibold text-slate-700">Restaurants</h2>
 
-      <div className="my-4">
-        <Link to="new">
-          <Button type="button" primary>
-            Add Restaurant
-          </Button>
-        </Link>
-      </div>
+			<div className="my-4">
+				{restaurants.map(({ id, name }) => (
+					<div key={`${id}-${name}`}>
+						<Link to={`${id}/cuisine`}>{name}</Link>
+					</div>
+				))}
+			</div>
 
-      <Outlet />
-    </div>
-  )
+			<div className="my-4">
+				<Link to="new">
+					<Button type="button" primary>
+						Add Restaurant
+					</Button>
+				</Link>
+			</div>
+
+			<Outlet />
+		</div>
+	)
 }
