6907062f3d45dcda52153e8bcbc992555bd68d4a
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-16 18:27:41 +0500

    feat(main): add input field to filter restaurants

--- apps/main/app/models/restaurant.server.js
+++ apps/main/app/models/restaurant.server.js
@@ -26,11 +26,34 @@ export function getRestaurant(id) {
       id: true,
       name: true,
       address: true,
-      menu: {
+      Menu: {
         select: {
           id: true,
         },
       },
     },
   });
+}
+
+export function filterRestaurants({ searchQuery }) {
+  const filterBy = `%${searchQuery}%`;
+
+  return prisma.$queryRaw`
+    SELECT r.id,
+    r.name,
+    c.name as cuisine
+    FROM public."Restaurant" as r
+    left outer join public."Menu" as m
+    on r.id = m."restaurantId"
+    left outer join public."Dish" as d
+    on m.id = d."menuId"
+    left outer join public."RestaurantCuisine" as rc
+    on r.id = rc."restaurantId"
+    left outer join public."Cuisine" as c
+    on c.id = rc."cuisineId"
+    WHERE d.name ilike ${filterBy}
+    or r.name ilike ${filterBy}
+    or c.name ilike ${filterBy}
+    group by r.id, r.name, c.name
+  `;
 }
--- apps/main/app/routes/index.jsx
+++ apps/main/app/routes/index.jsx
@@ -1,37 +1,56 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { getRestaurants } from "~/models/restaurant.server";
+import { Form, useLoaderData } from "@remix-run/react";
+import { filterRestaurants } from "~/models/restaurant.server";
 
-export async function loader() {
-  const restaurants = await getRestaurants();
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const searchQuery = url.searchParams.get("search");
+
+  const restaurants = await filterRestaurants({
+    searchQuery,
+  });
   return json({ restaurants });
 }
 
 export default function Index() {
   const { restaurants } = useLoaderData();
-  console.log({ restaurants });
+
   return (
     <div
       style={{
         fontFamily: "system-ui, sans-serif",
         lineHeight: "1.5",
-        boxSizing: "border-box",
       }}
     >
       <div className="flex flex-1 bg-emerald-400 p-2 text-white">
         Pak Restaurants
       </div>
-      <div className="mt-8 grid grid-cols-12 box-content">
-        <div className="col-span-12 flex-1 p-2">
+      <div className="mt-8 grid grid-cols-12 p-2">
+        <Form className="col-span-12" method="get">
           <input
             id="search"
             type="text"
             name="search"
-            className="bg-white w-full p-2 border-2 rounded outline-0 hover:border-emerald-400 focus:border-emerald-400"
-            placeholder="Search dishes, cuisine, restaurants..."
+            className="w-full p-2 bg-white border-2 rounded outline-0 hover:border-emerald-400 focus:border-emerald-400"
+            placeholder="Kya khaney ka mood hai?"
             autoFocus
           />
-        </div>
+        </Form>
+      </div>
+
+      <div className="my-8 h-0.5 w-full bg-slate-100" />
+
+      <div className="p-2 m-2">{restaurants.length} Restaurants found</div>
+
+      <div className="grid p-2">
+        {restaurants.map(({ id, name }) => (
+          <div
+            key={id}
+            className="my-1 p-2 cursor-pointer border-b-2 text-slate-700 hover:shadow-md"
+          >
+            {name}
+          </div>
+        ))}
       </div>
     </div>
   );
