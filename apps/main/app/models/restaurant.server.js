02b36e665ce7dfc7366647288736a2e55cce080c
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-16 20:07:49 +0500

    feat(main): add restaurant page

--- apps/main/app/models/restaurant.server.js
+++ apps/main/app/models/restaurant.server.js
@@ -1,25 +1,6 @@
 import { prisma } from "db";
 
-export function getRestaurants() {
-  return prisma.restaurant.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-    orderBy: { updatedAt: "desc" },
-  });
-}
-
-export function createRestaurant({ name, address }) {
-  return prisma.restaurant.create({
-    data: {
-      name,
-      address,
-    },
-  });
-}
-
-export function getRestaurant(id) {
+export function getRestaurantById(id) {
   return prisma.restaurant.findFirst({
     where: { id },
     select: {
@@ -29,6 +10,22 @@ export function getRestaurant(id) {
       Menu: {
         select: {
           id: true,
+          Dish: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+      RestaurantCuisine: {
+        select: {
+          Cuisine: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
         },
       },
     },
---
+++ apps/main/app/routes/restaurants/$restaurantId.jsx
@@ -0,0 +1,33 @@
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { getRestaurantById } from "~/models/restaurant.server";
+
+export async function loader({ params }) {
+  const { restaurantId } = params;
+  const restaurant = await getRestaurantById(restaurantId);
+  return json({ restaurant });
+}
+
+export default function RestaurantPage() {
+  const { restaurant } = useLoaderData();
+  const { name, address, Menu, RestaurantCuisine } = restaurant;
+  const [Dish] = Menu;
+
+  return (
+    <div>
+      <div className="flex flex-1 bg-emerald-400 p-3 text-white">
+        Kia khana hai?
+      </div>
+
+      <div className="grid bg-white py-4 px-4 rounded max-w-xl mx-auto justify-items-center my-8">
+        <h1 className="my-2 text-xl font-semibold uppercase">{name}</h1>
+        {RestaurantCuisine.map(({ Cuisine }, index) => (
+          <div key={Cuisine.id} className="text-sm my-2 font-light">
+            {Cuisine.name} {!index === 0 && "/"}
+          </div>
+        ))}
+        <div className="text-sm my-2 font-light">{address}</div>
+      </div>
+    </div>
+  );
+}
