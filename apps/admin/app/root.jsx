d0d9fb322ac0beadd699e767c1b766c7dde1e6c7
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-17 22:03:04 +0500

    fix(admin): remove sidebar

--- apps/admin/app/root.jsx
+++ apps/admin/app/root.jsx
@@ -1,42 +1,39 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from '@remix-run/react'
 
-import Sidebar from "~/components/Sidebar";
+import Sidebar from '~/components/Sidebar'
 
-import styles from "./styles/tailwind.css"
+import styles from './styles/tailwind.css'
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }]
+	return [{ rel: 'stylesheet', href: styles }]
 }
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+	charset: 'utf-8',
+	title: 'New Remix App',
+	viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body className="h-full">
-        <div className="flex">
-          <Sidebar />
-          <Outlet />
-        </div>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body className="h-full bg-slate-100">
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	)
 }
