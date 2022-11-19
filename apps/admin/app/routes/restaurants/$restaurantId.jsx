f8631cd1d2773600ae8de0457cb3248258cb60ce
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-19 19:40:55 +0500

    fix(admin): change restaurant page tab style

--- apps/admin/app/routes/restaurants/$restaurantId.jsx
+++ apps/admin/app/routes/restaurants/$restaurantId.jsx
@@ -28,11 +28,11 @@ export default function RestaurantPage() {
 			</div>
 
 			<div className="max-w-lg py-3 px-8 mx-auto rounded">
-				<div className="my-8 grid grid-cols-12 grid-flow-col justify-between border-2 divide-x-2">
+				<div className="my-8 grid grid-cols-12 gap-1 grid-flow-col shadow">
 					<NavLink
 						className={({ isActive }) =>
-							`${isActive && 'bg-white text-emerald-400'}
-              p-2 col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
+							`${isActive && 'text-emerald-400 border-b-2 border-b-emerald-400'}
+              p-2 bg-white col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
             `
 						}
 						to="details"
@@ -41,8 +41,10 @@ export default function RestaurantPage() {
 					</NavLink>
 					<NavLink
 						className={({ isActive }) =>
-							`${isActive && 'bg-white text-emerald-400'}
-              p-2 col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
+							`${
+								isActive && ' text-emerald-400 border-b-2 border-b-emerald-400'
+							}
+              p-2 bg-white col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
             `
 						}
 						to="cuisines"
@@ -51,8 +53,8 @@ export default function RestaurantPage() {
 					</NavLink>
 					<NavLink
 						className={({ isActive }) =>
-							`${isActive && 'bg-white text-emerald-400'}
-             p-2 col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
+							`${isActive && 'text-emerald-400 border-b-2 border-b-emerald-400'}
+             p-2 bg-white col-span-4 text-center hover:text-emerald-300 active:text-emerald-500
             `
 						}
 						to={`menu/${restaurant.menus[0].id}`}
