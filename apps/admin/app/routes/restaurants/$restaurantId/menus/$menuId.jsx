bea7116c5dfd6d2868698a83b92fa1088221fb94
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-22 23:42:04 +0500

    refactor(admin): remove extra categories folder

--- apps/admin/app/routes/restaurants/$restaurantId/menus/$menuId.jsx
+++ apps/admin/app/routes/restaurants/$restaurantId/menus/$menuId.jsx
@@ -1,40 +1,28 @@
-import {
-	Outlet,
-	Link,
-	useSearchParams,
-	useOutletContext,
-} from '@remix-run/react'
-import { redirect } from '@remix-run/node'
-
-import Button from '~/components/Button'
-import AddDishDialog from '~/dialogs/AddDish'
-
-import { createDish } from '~/models/dish.sever'
-
-export async function action({ request, params }) {
-	const { menuId, restaurantId } = params
-	const formData = await request.formData()
-	const name = formData.get('dish-name')
-	await createDish({ name, menuId })
-
-	return redirect(`/restaurants/${restaurantId}/menu/${menuId}`)
-}
+import { Outlet, Link, useOutletContext } from '@remix-run/react'
 
 export default function RestaurantMenu() {
 	const { restaurant } = useOutletContext()
 	const { menus } = restaurant
-	const [searchParams] = useSearchParams()
-	const showNewDishModal = searchParams.get('new-dish')
 
 	return (
 		<div>
 			<div>
+				{menus[0].categories.length > 0 && (
+					<Link to={`categories/new`}>
+						<button
+							type="button"
+							className="font-bold border-b-2 text-slate-400 w-full text-left px-4 py-3 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
+						>
+							Add new category
+						</button>
+					</Link>
+				)}
 				{menus[0].categories.map((category) => (
 					<>
 						<Link
 							to={`categories/${category.id}`}
 							key={category.id}
-							className="block text-slate-700 font-semibold py-3 px-4 border-b-2 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
+							className="block text-slate-700 font-bold py-3 px-4 border-b-2 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
 						>
 							<span className="overflow-hidden whitespace-nowrap">
 								{category.name}
@@ -65,13 +53,12 @@ export default function RestaurantMenu() {
 			<Link to={`categories/new`}>
 				<button
 					type="button"
-					className="font-semibold text-slate-400 w-full text-left px-4 py-3 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
+					className="font-bold text-slate-400 w-full text-left px-4 py-3 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
 				>
 					Add new category
 				</button>
 			</Link>
 			<Outlet />
-			{showNewDishModal && <AddDishDialog title={'Add dish'} />}
 		</div>
 	)
 }
