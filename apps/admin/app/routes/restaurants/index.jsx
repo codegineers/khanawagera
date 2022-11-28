a72ee07f6d9c7c18762493e76bc9776debfb01be
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-28 14:48:38 +0500

    fix(admin): add new restaurant button at the top if list is greater than 0

--- apps/admin/app/routes/restaurants/index.jsx
+++ apps/admin/app/routes/restaurants/index.jsx
@@ -15,6 +15,16 @@ export default function RestaurantPage() {
 			<div className="bg-emerald-400 py-3 px-2 text-white">Restaurants</div>
 
 			<div className="max-w-md mx-auto my-8 bg-white rounded shadow">
+				{restaurants.length > 0 && (
+					<Link to="new">
+						<button
+							type="button"
+							className="font-bold text-slate-400 border-b-2 w-full text-left px-4 py-3 active:bg-emerald-500 border-slate-100 hover:bg-emerald-300 hover:text-white cursor-pointer"
+						>
+							Add new restaurant
+						</button>
+					</Link>
+				)}
 				<div>
 					{restaurants.map(({ id, name }) => (
 						<Link
