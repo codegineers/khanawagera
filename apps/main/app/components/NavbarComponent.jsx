eb3bf1bda2e1e2f25de4caa8b4c348ea32ed1040
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-29 14:31:07 +0500

    refactor(main): extract navbar in a separate component

---
+++ apps/main/app/components/NavbarComponent.jsx
@@ -0,0 +1,30 @@
+import { Link, Form, useSearchParams } from "@remix-run/react";
+
+export default function NavbarComponent() {
+  const [searchParams] = useSearchParams();
+  const params = searchParams.get("q") || searchParams.get("cuisine");
+
+  return (
+    <>
+      <Link
+        to="/"
+        className="flex flex-1 font-bold bg-emerald-400 p-3 text-white"
+      >
+        KW
+      </Link>
+      <div className="grid grid-cols-12 p-3 bg-emerald-400">
+        <Form className="col-span-12" action="/search">
+          <input
+            id="search"
+            type="text"
+            name="q"
+            className="w-full p-2 bg-white border-2 rounded outline-0 hover:border-emerald-400 focus:border-emerald-400"
+            placeholder="Kya khaney ka mood hai?"
+            value={params}
+            required
+          />
+        </Form>
+      </div>
+    </>
+  );
+}
