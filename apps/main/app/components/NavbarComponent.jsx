ab29c7a82e07ba8ca0100f9cff3bb3a507f20866
Saud Punjwani <saudpunjwani101@gmail.com>
2022-11-29 16:14:59 +0500

    fix(main): reduce search field size and center it

--- apps/main/app/components/NavbarComponent.jsx
+++ apps/main/app/components/NavbarComponent.jsx
@@ -5,26 +5,23 @@ export default function NavbarComponent() {
   const params = searchParams.get("q") || searchParams.get("cuisine");
 
   return (
-    <>
-      <Link
-        to="/"
-        className="flex flex-1 font-bold bg-emerald-400 p-3 text-white"
-      >
+    <div className="px-4 py-2 bg-emerald-400 shadow">
+      <Link to="/" className="block font-bold bg-emerald-400 text-white">
         KW
       </Link>
-      <div className="grid grid-cols-12 p-3 bg-emerald-400">
-        <Form className="col-span-12" action="/search">
+      <div className="my-2 max-w-md mx-auto bg-emerald-400">
+        <Form action="/search">
           <input
             id="search"
             type="text"
             name="q"
-            className="w-full p-2 bg-white border-2 rounded outline-0 hover:border-emerald-400 focus:border-emerald-400"
+            className="w-full p-2 shadow-md rounded outline-0"
             placeholder="Kya khaney ka mood hai?"
             value={params}
             required
           />
         </Form>
       </div>
-    </>
+    </div>
   );
 }
