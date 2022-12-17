b25a8e0c99abd6123343cf73576322637656bb71
Saud Punjwani <saudpunjwani101@gmail.com>
2022-12-17 15:22:46 +0500

    refactor(admin): change new restaurant page to support ts

--- apps/admin/app/routes/restaurants/new.tsx
+++ apps/admin/app/routes/restaurants/new.tsx
@@ -1,14 +1,29 @@
 import { Form } from '@remix-run/react'
-import { redirect } from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
+import type { ActionArgs } from '@remix-run/node'
 import { createRestaurant } from '~/models/restaurant.server'
 import { createMenu } from '~/models/menu.server'
 
 import Button from '~/components/Button'
 
-export async function action({ request }) {
+export async function action({ request }: ActionArgs) {
 	const formData = await request.formData()
 	const name = formData.get('name')
 	const address = formData.get('address')
+
+	if (typeof name !== 'string' || name.length === 0) {
+		return json(
+			{ errors: { name: 'Name is required', address: null } },
+			{ status: 400 }
+		)
+	}
+
+	if (typeof address !== 'string' || address.length === 0) {
+		return json(
+			{ errors: { address: 'Address is required', name: null } },
+			{ status: 400 }
+		)
+	}
 
 	const restaurant = { name, address }
 	const newRestaurant = await createRestaurant(restaurant)
@@ -41,14 +56,13 @@ export default function NewRestaurantPage() {
 					<label className="block text-slate-700">Address</label>
 					<textarea
 						className="w-96 border-2 px-2 py-1 mt-2"
-						type="text"
 						name="address"
 						required
 					/>
 				</div>
 				<div className="mt-8">
 					<Button type="submit" primary>
-						Create
+						<span>Create</span>
 					</Button>
 				</div>
 			</Form>
